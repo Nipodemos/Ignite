cd7bfa66ef312d242ee006f523007deef71516df
Nipodemos <nipodemos13@gmail.com>
2021-03-09 21:10:20 -0300

    feat: :sparkles: Configurando o projeto - Utilizando métodos HTTP

--- nodejs/fundamentos/src/index.js
+++ nodejs/fundamentos/src/index.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello world")
+app.get("/courses", (_, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3" ]);
 });
+
+app.post("/courses", (_, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.put("/courses/:id", (request, response)=>{
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.patch("/courses/:id", (request, response)=>{
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"])
+})
+
+app.delete("/courses/:id", (request, response)=>{
+  return response.json([ "Curso 7", "Curso 3", "Curso 4"])
+})
 
 app.listen(3333, () => {
   console.log("Servidor iniciado em http://localhost:3333");
