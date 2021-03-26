78972560f1740f2d8d89d0e3ef966ccc0ecbaf73
Nipodemos <nipodemos13@gmail.com>
2021-03-26 11:29:38 -0300

    test: :sparkles: Criando a API - Debugando a aplicação

--- 03-criando-api-com-typescript/src/server.ts
+++ 03-criando-api-com-typescript/src/server.ts
@@ -2,7 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => response.json({ message: 'hello world!' }));
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () =>
   console.log('Servidor rodando no link http://localhost:3333'),
