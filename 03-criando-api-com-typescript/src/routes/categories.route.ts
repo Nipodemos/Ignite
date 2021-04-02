c99a8181f783d63eef7e739560c3ebc3c9b20de2
Nipodemos <nipodemos13@gmail.com>
2021-04-02 14:05:29 -0300

    feat: :sparkles: Criando a api - inserindo ID com uuid

--- 03-criando-api-com-typescript/src/routes/categories.route.ts
+++ 03-criando-api-com-typescript/src/routes/categories.route.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRouter = Router();
 const categories = [];
 
 categoriesRouter.post('/', (request, response) => {
   const { name, description } = request.body;
-  categories.push({ name, description });
+
+  const category = { name, description, id: uuidV4 };
+  categories.push(category);
   return response.status(201).send();
 });
 
