f395da544ecd00045362ec8f33d4be6597f264c7
Nipodemos <nipodemos13@gmail.com>
2021-04-09 17:46:32 -0300

    feat: :sparkles: Trabalhando com upload - criando upload de arquivos

--- 03-criando-api-com-typescript/src/routes/categories.route.ts
+++ 03-criando-api-com-typescript/src/routes/categories.route.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/use_cases/create_category';
 import listCategoriesController from '../modules/cars/use_cases/list_category';
 
 const categoriesRouter = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRouter.get('/', (request, response) => {
   listCategoriesController.handle(request, response);
@@ -13,4 +18,9 @@ categoriesRouter.post('/', (request, response) =>
   createCategoryController.handle(request, response),
 );
 
+categoriesRouter.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log('file :>> ', file);
+  response.send();
+});
 export default categoriesRouter;
