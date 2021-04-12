411040ed622ce3ae7b205753c85d3fd3765d39ef
Nipodemos <nipodemos13@gmail.com>
2021-04-12 18:16:24 -0300

    feat: :sparkles: Trabalhando com upload - conhecendo o conceito de stream

--- 03-criando-api-com-typescript/src/modules/cars/use_cases/import_category/import_category.usecase.ts
+++ 03-criando-api-com-typescript/src/modules/cars/use_cases/import_category/import_category.usecase.ts
@@ -1,9 +1,18 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import CategoriesRepository from '../../repositories/implementations/categories.repository';
 
 export default class ImportCategoryUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {}
 
-  execute(file: any) {
-    console.log('file :>> ', file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log('line :>> ', line);
+    });
   }
 }
