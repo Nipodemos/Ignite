1b84399fbc811267d9ac11e9c51bca97d2d354a4
Nipodemos <nipodemos13@gmail.com>
2021-04-16 14:10:53 -0300

    feat: :sparkles: Iniciando a documentação - Removendo os arquivos de upload

--- 03-criando-api-com-typescript/src/modules/cars/use_cases/import_category/import_category.usecase.ts
+++ 03-criando-api-com-typescript/src/modules/cars/use_cases/import_category/import_category.usecase.ts
@@ -25,6 +25,7 @@ export default class ImportCategoryUseCase {
         })
         .on('end', () => {
           resolve(categories);
+          fs.promises.unlink(file.path);
         })
         .on('error', (err) => {
           reject(err);
